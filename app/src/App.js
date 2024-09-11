92991d590d1831b1162983889a722d5248b0cc57
sschemist <Sathun@gmail.com>
2024-09-11 14:23:38 -0400

    implemented signin page

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import SignIn from "./sign-in/SignIn"
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SignIn/>
     </div>
   );
 }
+
 
 export default App;
