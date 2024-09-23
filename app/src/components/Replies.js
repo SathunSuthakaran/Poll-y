b3c0ae0e7193a2e6bd72fddd45ebfb41e327d116
sschemist <Sathun@gmail.com>
2024-09-23 11:46:57 -0400

    completed foundation of app

--- app/src/components/Replies.js
+++ app/src/components/Replies.js
@@ -29,10 +29,32 @@ const Replies = () => {
         fetchReplies();
     }, [id]);
 
-    //👇🏻 This function when triggered when we add a new reply
+    
+
+    const addReply = () => {
+        fetch("http://localhost:4000/api/create/reply", {
+            method: "POST",
+            body: JSON.stringify({
+                id,
+                userId: localStorage.getItem("_id"),
+                reply,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                alert(data.message);
+                navigate("/dashboard");
+            })
+            .catch((err) => console.error(err));
+    };
+    
     const handleSubmitReply = (e) => {
         e.preventDefault();
-        console.log({ reply });
+        //👇🏻 calls the function
+        addReply();
         setReply("");
     };
     return (
