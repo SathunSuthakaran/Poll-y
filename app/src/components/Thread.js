ce7c921bc219e7903367dd37387be900a57b4b7e
sschemist <Sathun@gmail.com>
2024-12-02 08:22:46 -0500

    found tree-view on MUI

---
+++ app/src/components/Thread.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+return(<Typography>
+  {lastClickedItem == null
+    ? 'No item click recorded'
+    : `Last clicked item: ${lastClickedItem}`}
+</Typography>
+<Box sx={{ minHeight: 352, minWidth: 250 }}>
+  <RichTreeView
+    items={MUI_X_PRODUCTS}
+    onItemClick={(event, itemId) => setLastClickedItem(itemId)}
+  />
+</Box>)
