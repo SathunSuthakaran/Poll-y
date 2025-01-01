16b4d0736d7cfe41533c5548f59a3918af5a81ac
sschemist <Sathun@gmail.com>
2024-12-31 22:05:50 -0500

    added state to thread

--- app/src/components/Thread.js
+++ app/src/components/Thread.js
@@ -1,14 +1,22 @@
-import React from "react"
+import React, { useState } from "react";
 
-return(<Typography>
-  {lastClickedItem == null
-    ? 'No item click recorded'
-    : `Last clicked item: ${lastClickedItem}`}
-</Typography>
-<Box sx={{ minHeight: 352, minWidth: 250 }}>
-  <RichTreeView
-    items={MUI_X_PRODUCTS}
-    onItemClick={(event, itemId) => setLastClickedItem(itemId)}
-  />
-</Box>)
+const Thread = () => {
+  const [lastClickedItem, setLastClickedItem] = useState(null);
 
+  return (
+    <main className="Thread">
+      <Typography>
+        {lastClickedItem == null
+          ? 'No item click recorded'
+          : `Last clicked item: ${lastClickedItem}`}
+      </Typography>
+      <Box sx={{ minHeight: 352, minWidth: 250 }}>
+        <RichTreeView
+          items={MUI_X_PRODUCTS}
+          onItemClick={(event, itemId) => setLastClickedItem(itemId)}
+        />
+      </Box>
+    </main>
+  );
+};
+export default Thread;
